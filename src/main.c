d55bd56faf7c80296e0e6f17ea0d795c12dffdf2
OmarMohamedElsheikh <omarmohammedomarali@gmail.com>
2026-08-03 22:11:47 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+char *builtins[] = {"echo", "exit" , "type"};
+
+int contains(char *arr[], int size, const char *target) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(arr[i], target) == 0)
+            return 1;   
+    }
+    return 0;           
+}
+
 
 int main(int argc, char *argv[]) {
   char command[30];
@@ -19,14 +31,27 @@ int main(int argc, char *argv[]) {
 	  	command[len - 1] = '\0';
 	  }
 
-	  if(!(strcmp(command, "exit"))){
+	  char *start = command;
+	  while (isspace((unsigned char)*start))
+	      start++;
+	  
+	  size_t lenc = strlen(start);
+	  while (len > 0 && isspace((unsigned char)start[lenc - 1]))
+	      start[--lenc] = '\0';
+	      
+	  if(!(strcmp(start, "exit"))){
 	  	break;
-	  };
-
-	  if (strncmp(command, "echo ", 5) == 0) {
-	      printf("%s\n", command + 5);
+	  }else if (strncmp(start, "echo ", 5) == 0) {
+	      printf("%s\n", start + 5);
+	  }else if (strncmp(start, "type ", 5) == 0){
+	  	int size = sizeof(builtins) / sizeof(builtins[0]);
+	  	if (contains(builtins , size , start+5)){
+	  		printf("%s is a shell builtin\n", start+5);
+	  	}else{
+	  		printf("%s: not found\n", start+5);
+	  	}
 	  }else{
-	  printf("%s: command not found\n", command);
+	  printf("%s: command not found\n", start);
 	  }
 	};
   return 0;
